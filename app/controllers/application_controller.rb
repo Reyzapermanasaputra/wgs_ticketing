b8e58bdf6816b595dc8084288e5bf557d6a0c4e0
Reyza Permana <reyza.permana@wgs.co.id>
2018-02-08 23:10:50 +0700

    add page dashboard

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  
+  private
+
+  #Overwrite the sign out redirect path
+  def after_sign_out_path_for(resource_or_scope)
+  	user_session_path
+  end
+
 end
